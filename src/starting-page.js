f8f3b5aa82dc02613aee6ed649f0fa38c04df3ea
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-01-23 14:37:35 +1100

    added loaders for css and images file, and some more initial page load components

--- src/starting-page.js
+++ src/starting-page.js
@@ -1,8 +1,25 @@
+import './style.css';
+
 const startingComponent = () => {
     const content = document.querySelector("#content");
     const header = document.createElement('div');
     header.setAttribute("id", "header");
-    header.textContent = "Hey!";
+    // header.textContent = "Hey!";
+
+    const home = document.createElement('span');
+    home.setAttribute("id", "home-btn");
+    home.textContent = "Home";
+    header.appendChild(home);
+
+    const menu = document.createElement('span');
+    menu.setAttribute("id", "menu-btn");
+    menu.textContent = "Menu";
+    header.appendChild(menu);
+
+    const contact = document.createElement('span');
+    contact.setAttribute("id", "contact-btn");
+    contact.textContent = "Contact";
+    header.appendChild(contact);
 
     content.appendChild(header);
 }
