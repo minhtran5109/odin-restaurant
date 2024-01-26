31945e9502670cf38b25246097c0a05b5ccda6a5
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-01-26 17:26:12 +1100

    added contact form and styling

--- src/contact-page.js
+++ src/contact-page.js
@@ -5,7 +5,51 @@ const contactComponent = () => {
     const contactContent = document.createElement('div');
     contactContent.setAttribute("id", "contact-content");
 
-    contactContent.textContent = "Contact Placeholder.";
+    // lazy
+    contactContent.innerHTML= `
+        <p>Visit us at (adress) or call us at 04XX XXX XXX</p>
+        <p>We offer full-service catering for any event.</p>
+        <br>
+        <p><b>RESERVE A TABLE TODAY:</b></p>
+        <form action="/action_page.php">
+        <div class="row">
+          <div class="col-25">
+            <label for="name">Your Name</label>
+          </div>
+          <div class="col-75">
+            <input type="text" id="name" name="name" placeholder="Your name...">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25">
+            <label for="npeople">Number of people</label>
+          </div>
+          <div class="col-75">
+            <input type="number" id="npeople" name="npeople" placeholder="Number of people...">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25">
+            <label for="birthday">Date</label>
+          </div>
+          <div class="col-75">
+            <input type="date" id="birthday" name="birthday">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25">
+            <label for="smessage">Additional Note</label>
+          </div>
+          <div class="col-75">
+            <textarea id="smessage" name="smessage" placeholder="Any additional message/special requirement" style="height:200px"></textarea>
+          </div>
+        </div>
+        <br>
+        <div class="row">
+          <input type="submit" value="BOOK">
+        </div>
+        </form>
+    `;
 
     content.appendChild(contactContent);
 }
