5b91349afdacf5a0e737cf39a929b68cd120b183
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-01-26 16:44:09 +1100

    additional general styling

--- src/home-page.js
+++ src/home-page.js
@@ -5,7 +5,13 @@ const homeComponent = () => {
     const homeContent = document.createElement('div');
     homeContent.setAttribute("id", "home-content");
 
-    homeContent.textContent = "MAJOR-OH.\nIt's all about the food. We're on a mission to deliver food straight to your mouth."
+    const name = document.createElement('h1');
+    name.textContent = "MAJOR-OH";
+    const text = document.createElement('p')
+    text.textContent = "It's all about the food. We're on a mission to deliver food straight to your mouth."
+
+    homeContent.appendChild(name);
+    homeContent.appendChild(text);
 
     content.appendChild(homeContent);
 }
